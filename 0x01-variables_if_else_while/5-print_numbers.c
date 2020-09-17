84dcf93b97111b3f0c061b5d2019ed76a6d7ea23
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-17 10:28:49 +0000

    First commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums <= 9; nums++)
+		printf("%d", nums);
+	printf("\n");
+	return (0);
+}
