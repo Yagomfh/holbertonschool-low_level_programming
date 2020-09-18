8138a73e2d48df48b3e1aaf8d25d8c50ecf1df62
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-18 08:10:26 +0000

    101-quote.c answer

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,4 +7,29 @@
  */
 int main(void)
 {
+	int a, b;
+
+	a = 0;
+	b = 0;
+	while (a < 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
 }
