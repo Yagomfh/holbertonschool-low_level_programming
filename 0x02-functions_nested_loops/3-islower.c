3c82b3dfcf070e6c6567d2ac887c2552aadaf92f
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-22 07:54:26 +0000

    Fixing 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,7 @@
 
 /**
 * _islower - function that checks for lowercase character
+* @c: character to check
 *
 * Return: 1 if c is lowercase & 0 otherwise
 */
@@ -12,7 +13,7 @@ int _islower(int c)
 {
 	int result;
 
-	if (islower(c) > 0)
+	if (c > 'a' && c < 'z')
 		result = 1;
 	else
 		result = 0;
