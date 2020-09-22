749d4c38406389b25e9b23dca0d4ea8b1b2e03c1
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-22 07:59:00 +0000

    Fixing 3-islower.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,6 +4,7 @@
 
 /**
 * _isalpha - function that checks for alphabetic character
+* @c: char to check
 *
 * Return: 1 if c is a letter, lowercase or uppercase & 0 otherwise
 */
@@ -11,7 +12,7 @@
 int _isalpha(int c)
 {
 	int result;
-	if (isalpha(c) > 0)
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 		result = 1;
 	else
 		result = 0;
