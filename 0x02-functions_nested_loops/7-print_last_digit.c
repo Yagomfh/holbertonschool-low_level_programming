345f083782bc50f50c72383d3bc072220ce30407
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-22 10:03:46 +0000

    Fixing errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit of an int
- * @n: the int number 
+ * @n: the int number
  *
  * Return: the value of the last digit
  */
@@ -16,4 +16,3 @@ int print_last_digit(int n)
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
-
