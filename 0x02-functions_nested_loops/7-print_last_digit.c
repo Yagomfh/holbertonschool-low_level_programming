540cfb114e10e6847edb51886afea0186208bea1
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-22 08:32:14 +0000

    Fixing 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
 * print_last_digit - function that prints the last digit of a number
@@ -12,8 +11,9 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
+	{
 		n = -1 * n;
-
+	}
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
