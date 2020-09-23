63875c19bf4aea68b784767d929928550536d925
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-23 08:21:48 +0000

    101-natural.c answer

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - prints Holberton, followed by a new line
+ * main - sum of all the multiples of 3 or 5 below 1024
  *
  * Return: 0 when it works
  */
 
 int main(void)
 {
+	int i;
+	int result = 0;
+
+	for (i = 1; i < 1024; i++)
+		if ((i % 3 == 0) || (i % 5 == 0))
+			result = result + i;
+	printf("%d \n", result);
+	return (0);
 }
