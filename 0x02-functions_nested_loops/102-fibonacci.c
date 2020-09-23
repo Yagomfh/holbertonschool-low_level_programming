e84173441bf8593ab38f4e72dee4824041160fd1
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-23 09:00:47 +0000

    102-fibonacci.c answer

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -9,4 +8,19 @@
 
 int main(void)
 {
+	long long int f0 = 0;
+	long long int f1 = 1;
+	long long int fn, n;
+
+	for (n = 2; n <= 50; n++)
+	{
+		printf("%lli", f0);
+		fn = f0 + f1;
+		f0 = f1;
+		f1 = fn;
+		if (n != 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 }
