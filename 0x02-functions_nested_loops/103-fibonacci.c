5c155e1d9c42c7b657906af657958d580a12c4fb
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-23 09:30:24 +0000

    103-fibonacci.c answer

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,4 +9,19 @@
 
 int main(void)
 {
+	unsigned long result = 0;
+	unsigned long f0 = 1;
+	unsigned long f1 = 2;
+	unsigned long fn;
+	int n;
+
+	for (n = 1; f0 <= 4000000; n++)
+	{
+		result = result + f0;
+		fn = f0 + f1;
+		f0 = f1;
+		f1 = fn;
+	}
+	printf("%lu\n", result);
+	return (0);
 }
