d14e1cc342cc02a2788594774ed3dee06d1fe5f3
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-23 08:29:30 +0000

    101-natural.c answer

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,11 @@
 int main(void)
 {
 	int i;
-	int result = 0;
+	long int result = 0;
 
 	for (i = 1; i < 1024; i++)
 		if ((i % 3 == 0) || (i % 5 == 0))
 			result = result + i;
-	printf("%d \n", result);
+	printf("%ld \n", result);
 	return (0);
 }
