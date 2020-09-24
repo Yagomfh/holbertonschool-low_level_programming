c1dc7ede149806ef8456e28c8a9a3c4951cb61a7
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-24 12:22:31 +0000

    9-fizz_buzz.c answer

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main - Fizz-Buzz
+*
+* Return: 0 when it works
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num == 100)
+		{
+			printf("Buzz\n");
+		}
+		else if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{ 
+			printf("%d ", num);
+		}
+	}
+	return (0);
+}
