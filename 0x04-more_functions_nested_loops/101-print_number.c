724bbcb86eaeac3921be50e780bfee68c43e0510
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-25 14:32:08 +0000

    Finallygit add 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
 * print_number - prints an integer
@@ -9,12 +10,29 @@
 
 void print_number(int n)
 {
+	int len, a, rem, i, bign;
+	a = n;
+	len = 0;
+	bign = 1;
+
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
-	if (n / 10)
-		print_number(n / 10);
+	while (a != 0)
+	{
+		rem = a % 10;
+		a = (a - rem) / 10;
+		len = len + 1;
+	}
+
+	for (i = 1; i < len; i++)
+		bign = bign * 10;
+
+	for (i = bign ; i > 1; i = i / 10)
+	{
+		_putchar(((n / i) % 10) + '0');
+	}
 	_putchar((n % 10) + '0');
 }
