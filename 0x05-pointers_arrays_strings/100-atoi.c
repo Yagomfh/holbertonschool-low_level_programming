7294ceac6d180674d57bdacc134e27affdc70d50
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-09-29 16:09:43 +0000

    100-atoi.c answer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strlen - the length of a string
+* @str: the string
+*
+* Return: the lenght of the string
+*/
+
+int _strlen(char *str)
+{
+	int result = 0;
+
+	while (str[result] != '\0')
+	{
+		result++;
+	}
+	return (result);
+}
+
+/**
+* _atoi - convert a string to an integer
+*
+* @s: string to convert
+*/
+
+int _atoi(char *s)
+{
+	int len = _strlen(s) - 1;
+	int i = 0;
+	int a;
+	int sign = 1;                                                            
+	int lenint = 0;                                                          
+	int result = 0;                                                          
+	unsigned int bign = 1;
+
+	while ((s[i] < '0') || (s[i] > '9'))
+	{
+		if (s[i] == '-')
+		{
+			sign = -1 * sign;
+		}
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9'))
+	{
+		lenint = lenint + 1;
+		i++;
+	}
+	for (a = 1; a < lenint; a++)
+	{
+		bign = bign * 10;
+	}
+	for (a = i - lenint; a <= i; a++)
+	{
+		result = result + ((s[a] - 48) * bign);
+		bign = bign / 10;
+	}
+	if (sign < 0)
+	{
+		result = -1 * result;
+	}
+	return (result);
+}
