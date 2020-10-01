7a487fb7355c3e5739d79d24eed244deb4f98702
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-01 16:27:46 +0000

    fix(l19 & l20) : adding -i to equation

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,11 +13,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int s;
 
-	for (i = 0; i < (n - 1) / 2; i++)
+	for (i = 0; i <= (n - 1) / 2; i++)
 	{
 		s = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = s;
-		n--;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = s;
 	}
 }
