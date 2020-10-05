8a38242db11fe67aa897bf46848692c8d1464924
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-05 06:58:34 +0000

    feat(0-memset.c) : first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _memset - fills memory with a constant byte
+* @s: memory area
+* @b: constant byte
+* @n: number of bytes
+*
+* Return: pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
