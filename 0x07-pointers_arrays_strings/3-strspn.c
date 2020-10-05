3806d0d2c9a2f587f9fb060167c3fd454f3b0a6f
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-05 07:57:03 +0000

    feat(3-strspn.c) : first commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: string to be scanned
+* @accept: string containing the 
+* characters to match
+*
+* Return: the number of characters matching
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int result = 0;
+	unsigned int i = 0;
+	unsigned int a;
+
+	while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+	{
+		a = 0;
+		while (accept[a] != 0)
+		{
+			if (s[i] == accept[a])
+				result++;
+			a++;
+		}
+		i++;
+	}
+	return (result);
+}
