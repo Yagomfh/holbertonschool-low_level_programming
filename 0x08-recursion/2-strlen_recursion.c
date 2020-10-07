4a7b90acaf8e16f02dae10763f3ed36b45c9b1b7
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-07 08:17:05 +0000

    feat(2-strlen_recursion.c) : first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: string to scan
+*
+* Return: the lenght of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != 0)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
