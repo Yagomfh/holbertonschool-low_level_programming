93a8f210ca94360052aae8eb4bd23b070ce2e5f9
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-07 10:39:35 +0000

    fix(1-print_rev_recursion.c) : betty complaint

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+* _print_rev_recursion - prints a string in reverse
+* @s: string to be printed
+*
+* Return: always 0
+*/
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == 0)
