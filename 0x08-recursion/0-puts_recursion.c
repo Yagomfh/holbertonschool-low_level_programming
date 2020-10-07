5878d1c0734e02ac6cde947d3a4242d8c131e3ec
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-07 10:29:28 +0000

    fix(0-puts_recursion.c) : recursion outside if

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	if (*s != 0)
-		_puts_recursion(s + 1);
-	else
+	if (*s == 0)
+	{
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
