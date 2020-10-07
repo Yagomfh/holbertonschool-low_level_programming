1ef87c85873a03b8579be742502b8a8ec245471c
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-07 10:23:58 +0000

    fix(0-puts_recursion.c) : single function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,6 @@
 #include "holberton.h"
 
 /**
-* _strlen_recursion - returns the length of a string
-* @s: string to scan
-*
-* Return: the lenght of the string
-*/
-
-int _strlen_recursion(char *s)
-{
-	if (*s != 0)
-		return (1 + _strlen_recursion(s + 1));
-	return (0);
-}
-
-/**
 * _puts_recursion - prints a string, followed by a new line
 * @s: the string to print
 *
@@ -23,11 +9,8 @@ int _strlen_recursion(char *s)
 
 void _puts_recursion(char *s)
 {
-	unsigned int len = _strlen_recursion(s);
-	unsigned int i = 0;
-
-	_putchar(s[i]);
-	if (i < len)
+	_putchar(*s);
+	if (*s != 0)
 		_puts_recursion(s + 1);
 	else
 		_putchar('\n');
