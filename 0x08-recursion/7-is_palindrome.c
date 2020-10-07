06a58731fa0e2b586e14811f42d5317f2522292a
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-07 12:21:46 +0000

    fix(7-is_palindrome.c) : betty complaint

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -14,20 +14,36 @@ int _strlen_recursion(char *str)
 	return (0);
 }
 
+/**
+* check_pal - check if string is palindrome
+* @a: index beginning of string
+* @b: index end of string
+* @y: the string to check
+*
+* Return: 1 if palindrom 0 if not
+*/
+
 int check_pal(int a, int b, char *y)
 {
 	if (y[a] != y[b])
 		return (0);
 	if (a > b)
 		return (1);
-	return(check_pal(a + 1, b - 1, y));
+	return (check_pal(a + 1, b - 1, y));
 }
+
+/**
+* is_palindrome - check if string is palindrome
+* @s: string to check
+*
+* Return: 1 if palindrom 0 if not
+*/
 
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s) - 1;
-	
+
 	if (*s == 0)
-		return(1);
+		return (1);
 	return (check_pal(0, len, s));
 }
