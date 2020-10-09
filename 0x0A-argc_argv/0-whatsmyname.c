c42af528ddb11a06607acf41545d9e400977f1f5
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-09 12:25:01 +0000

    feat(0-whatsmyname.c) : first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - prints program name + \n
+* @argc: argument count
+* @argv: argument values
+*
+* Return: always 0
+*/
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
