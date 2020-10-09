5e540616799c17f767865c7d4a72297415125736
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-09 13:32:55 +0000

    feat(3-mul.c) : first commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program that multiplies two numbers
+* @argc: argument count
+* @argv: argument values
+*
+* Return: 1 if argc != 3 & 0 otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
