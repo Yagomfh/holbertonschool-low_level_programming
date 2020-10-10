dfe37bfa6e00fe30078f811d91f0db0942112f72
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-10 17:28:37 +0000

    fix(4-add.c) : new if condition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
 * main - adds positive numbers
@@ -16,7 +15,7 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (isalpha(*argv[i]) != 0)
+		if (*argv[i] != '0' && atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
