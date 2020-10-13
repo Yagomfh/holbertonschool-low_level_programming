480f99760dcb98e5381841ca402a2687f724e0f3
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-13 13:22:13 +0000

    feat(4-free_grid.c) : first commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - frees a 2 dimensional grid previously created
+  * @grid: grid to free
+  * @height: height of grid
+  *
+  * Return: always 0
+  */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
