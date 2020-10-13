f8f78bb88970f0bfbdfc5c581f4ad581bd5ce88d
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-13 08:16:53 +0000

    feat(1-strdup.c) : first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _strlen - the length of a string
+* @s: the string
+*
+* Return: the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	int result = 0;
+
+	while (*s != '\0')
+	{
+		result++;
+		s++;
+	}
+	return (result);
+}
+
+/**
+  * _strdup - allocates space in memory containing a string
+  * @str: string to copy
+  *
+  * Return: pointer to allocated space
+  */
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int len = _strlen(str), i;
+
+	ar = malloc(len);
+
+	if (ar == '\0' || len <= 0)
+		return ('\0');
+	for (i = 0; i < len; i++)
+		ar[i] = str[i];
+	return (ar);
+}
