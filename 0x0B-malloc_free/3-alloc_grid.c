4913cad5bf311710808385a151bf58cc6a09b817
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-13 11:24:56 +0000

    fix(3-alloc_grid.c) : adding mem for NULL

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,12 +18,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ar = (int **)malloc(width * sizeof(int *));
+	ar = (int **)malloc((width + 1) * sizeof(int *));
 
 	if (ar == 0)
 		return (NULL);
 	for (i = 0; i < width; i++)
-		ar[i] = (int *)malloc(height * sizeof(int));
+		ar[i] = (int *)malloc((height + 1) * sizeof(int));
 	for (i = 0; i < width; i++)
 		for (j = 0; j < height; j++)
 			ar[i][j] = 0;
