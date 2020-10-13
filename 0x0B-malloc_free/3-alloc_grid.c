371ee6ba79fbcb9d3af3172fa0856bd7b798fec5
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-13 11:19:12 +0000

    feat(3-alloc_grid.c) : first commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - creates a 2 dimensional array of integers
+  * @width: width of the array
+  * @height: height of the array
+  *
+  * Return: NULL on failure & if width or height <= 0,
+  * a grid initialized at 0 otherwise
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ar;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ar = (int **)malloc(width * sizeof(int *));
+
+	if (ar == 0)
+		return (NULL);
+	for (i = 0; i < width; i++)
+		ar[i] = (int *)malloc(height * sizeof(int));
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+			ar[i][j] = 0;
+	return (ar);
+}
