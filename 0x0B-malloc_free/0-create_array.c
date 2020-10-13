f1e4d99c34c28aba34701fad88bbfee89df69ab1
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2020-10-13 10:37:45 +0000

    fix(0-create_array.c) : betty complaint

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *ar;
 	unsigned int i;
+
 	ar = malloc(size);
 
 	if (size == 0 || ar == 0)
-		return('\0');
+		return ('\0');
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 	return (ar);
