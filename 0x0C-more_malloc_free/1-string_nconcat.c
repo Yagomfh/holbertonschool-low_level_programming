7f7524dc73fd467e0707e189aebff56349389e0a
yagomfh <yagomfh@gmail.com>
2020-10-15 09:17:51 +0000

    feat(1-string_nconcat.c) : first commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _strlen - the length of a string
+* @s: the string
+*
+* Return: the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	int result = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (*s != '\0')
+	{
+		result++;
+		s++;
+	}
+	return (result);
+}
+
+/**
+  * string_nconcat - concatenates two strings
+  * @s1: 1st string
+  * @s2: 2nd string
+  * @n: bytes of s2 to treat
+  *
+  * Return: pointer to allocated space in memory or NULL if fails
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len_s1 = _strlen(s1);
+	unsigned int len_s2 = _strlen(s2);
+	unsigned int i;
+	char *ar;
+
+	printf("n = %u\n", n);
+	printf("len_s1 = %u\n", len_s1);
+	printf("len_s2 = %u\n", len_s2);
+	if (n <= len_s2)
+		ar = malloc((len_s1 + n + 1) * sizeof(char));
+	if (n > len_s2)
+		ar = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < len_s1; i++)
+		ar[i] = s1[i];
+	if (n <= len_s2)
+	{
+		for (i = 0; i < n; i++)
+			ar[i + len_s1] = s2[i];
+	}
+	if (n > len_s2)
+	{
+		for (i = 0; i < len_s2; i++)
+			ar[i + len_s1] = s2[i];
+	}
+	ar[i + len_s1] = '\0';
+	return (ar);
+}
