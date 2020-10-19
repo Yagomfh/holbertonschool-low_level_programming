e175883ce4c16b48c8f538c528ba4fe90290960f
yagomfh <yagomfh@gmail.com>
2020-10-19 13:36:22 +0000

    fix(4-new_dog.c) : freeing mem

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->name = name;
+	if (name != NULL)
+		my_dog->name = name;
+	else
+	{
+		return (NULL);
+		free(my_dog->name);
+	}
+	if (owner != NULL)
+		my_dog->owner = owner;
+	else
+	{
+		return (NULL);
+		free(my_dog->owner);
+	}
 	my_dog->age = age;
-	my_dog->owner = owner;
 	return (my_dog);
 }
