193551c03b08e5c38f719004a349b0dc8ea72afe
yagomfh <yagomfh@gmail.com>
2020-10-19 10:12:12 +0000

    feat(dog.h) : 1st commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
-#ifdef dog_h
-#define dog_h
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog 
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: properties of a dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
