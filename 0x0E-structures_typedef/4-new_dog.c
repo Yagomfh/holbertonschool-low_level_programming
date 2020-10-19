86446a626f71a9bb4ea576e7a83b4225385618ac
yagomfh <yagomfh@gmail.com>
2020-10-19 13:40:06 +0000

    fix(4-new_dog.c) : adding strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+
+/**
+* _strlen - the length of a string
+* @s: the string
+*
+* Return: the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	int result = 0;
+
+	while (*s != '\0')
+	{
+		result++;
+		s++;
+	}
+	return (result);
+}
+
+/**
+  * _strdup - allocates space in memory containing a string
+  * @str: string to copy
+  *
+  * Return: pointer to allocated space
+  */
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _strlen(str);
+	ar = malloc(len + 1 * sizeof(char));
+
+	if (ar == 0)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ar[i] = str[i];
+	return (ar);
+}
+
 /**
   * new_dog - creates a new dog
   * @name: name of dog
@@ -18,14 +63,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 	if (name != NULL)
-		my_dog->name = name;
+		my_dog->name = _strdup(name);
 	else
 	{
 		return (NULL);
 		free(my_dog->name);
 	}
 	if (owner != NULL)
-		my_dog->owner = owner;
+		my_dog->owner = _strdup(owner);
 	else
 	{
 		return (NULL);
