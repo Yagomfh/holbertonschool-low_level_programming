e6e79ea47f6749dfef6ea9c7aceef0ddd8ea79fb
yagomfh <yagomfh@gmail.com>
2020-10-21 10:49:18 +0000

    fix(1-array_iterator.c) : NULL condition

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,8 @@ void array_iterator(int *array, unsigned int size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (!array || !action)
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
