5d789e83b6ef7c0dc8752f475b7fd51e7de0aadd
yagomfh <yagomfh@gmail.com>
2020-10-23 08:03:24 +0000

    feat(1-print_numbers.c) : first commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: void or nothing if separator == NULL
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, unsigned int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
