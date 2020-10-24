0c59ed9c3d00604523d8560e0f1a896fc335920a
yagomfh <yagomfh@gmail.com>
2020-10-24 11:11:50 +0000

    fix(2-print_strings.c) : sep == null condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,6 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 
+	if (separator == NULL)
+		separator = "";
 	for (i = 0; i < n; i++)
 	{
 		tmp = va_arg(valist, char*);
