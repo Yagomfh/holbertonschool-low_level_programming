f52740b7cd6840469d536b1f1f954fdefb7ee8e8
yagomfh <yagomfh@gmail.com>
2020-10-24 08:47:26 +0000

    feat(3-print_all.c) : new headers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,16 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+#include <stdarg.h>
+
+typedef struct format
+{
+	char *str;
+	void (*f)(va_list);
+} format_t;
+
 void print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
 #endif
