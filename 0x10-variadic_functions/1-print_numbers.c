337e6c252a10a8bf25ec04f0230f671ef622e257
yagomfh <yagomfh@gmail.com>
2020-10-24 10:40:28 +0000

    feat(1-print_numbers.c) : va_arg int

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, unsigned int));
+		printf("%d", va_arg(valist, int));
 		if (i != n - 1)
 			printf("%s", separator);
 	}
