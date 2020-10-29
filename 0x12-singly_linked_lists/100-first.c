a72f0e44f844403db3f1b0a403bbfe0fd9c66dce
yagomfh <yagomfh@gmail.com>
2020-10-29 13:49:52 +0000

    fix(100-first.c) : betty complaint

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,14 @@
 #include "lists.h"
 
+/**
+ * laTortue - prints before main
+ * Return: void
+ */
+
 void laTortue(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *str1 = "You're beat! and yet, you must allow,\n";
+	char *str2 = "I bore my house upon my back!\n";
+
+	printf("%s%s", str1, str2);
 }
