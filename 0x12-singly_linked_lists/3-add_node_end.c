223ef5d0b99ca04cb58bf5b6fb296a1e765548fe
yagomfh <yagomfh@gmail.com>
2020-10-29 13:26:30 +0000

    fix(3-add_node_end.c) : malloc null

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -30,8 +30,12 @@ int _strlen(const char *s)
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
 	list_t *last = *head;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
