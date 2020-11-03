fc7e65d10ccb516a9eef46508a6b3679f508f039
yagomfh <yagomfh@gmail.com>
2020-11-03 09:31:01 +0000

    fix(101-pr..) : undo

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,7 +17,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (tmp < head)
 		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			break;
 		}
 		printf("[%p] %d\n", (void *)head, head->n);
 		tmp = (listint_t *)head;
