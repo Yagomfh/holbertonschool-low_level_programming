51ced10acb3a7ca631bfdc4b95c9fd7746eac60c
yagomfh <yagomfh@gmail.com>
2020-11-05 13:35:47 +0000

    feat(5-flip..) : 1st commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - number of bits you would need to flip
+ * to get from one number to another
+ * @n: first digit
+ * @m: second digit
+ * Return: number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int comparator = n ^ m;
+	unsigned int checker;
+	int index = 0;
+	int count = 0;
+
+	while (index < 64)
+	{
+		checker = 1;
+		checker = checker << index;
+		if ((comparator & checker) != 0)
+			count++;
+		index++;
+	}
+	return (count / 2);
+}
