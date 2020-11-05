2a20f3c3aac380bffc495bb42716054c30248036
yagomfh <yagomfh@gmail.com>
2020-11-05 12:06:49 +0000

    fix(2-get..) : betty + n=0 case

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,19 @@
 #include "holberton.h"
 
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to check
+ * @index: index to look for
+ * Return:  value of the bit at index index
+ * or -1 if an error occured
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit = (n >> index);
 
+	if (n == 0 && index == 0)
+		return (0);
 	if (bit == 0)
 		return (-1);
 	if ((bit & 1) == 0)
