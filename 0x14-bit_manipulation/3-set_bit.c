b7c15ed142fc7210992f5897578e852fc91835fd
yagomfh <yagomfh@gmail.com>
2020-11-05 12:59:27 +0000

    feat(3-set..) : 1st commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to number to modify
+ * @index: index to look for
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int modifier = 1;
+
+	if (index > 63)
+		return (-1);
+
+	*n = (*n) | (modifier << index);
+	return (1);
+}
