fa25f1d40737decfb041cf84cc9a9d34e9b543ef
yagomfh <yagomfh@gmail.com>
2020-11-09 12:37:19 +0000

    fix(1-crea..) : shorter version

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,24 +33,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-	{
-		fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-		if (fd == -1)
-			return (-1);
-		if (text_content != NULL)
-		{
-			writeT = write(fd, text_content, _strlen(text_content) + 1);
-			if (writeT == -1)
-				return (-1);
-		}
-	}
-	else
+		return (-1);
+	if (text_content)
 	{
 		writeT = write(fd, text_content, _strlen(text_content));
 		if (writeT == -1)
 			return (-1);
 	}
+	close(fd);
 	return (1);
 }
