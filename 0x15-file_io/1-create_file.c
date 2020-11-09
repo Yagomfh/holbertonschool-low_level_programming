11f5b13749c7050039b0d7c126ebbbfa5ab12d25
yagomfh <yagomfh@gmail.com>
2020-11-09 12:19:58 +0000

    feat(1-crea..) : 1st commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+* _strlen - the length of a string
+* @s: the string
+*
+* Return: the lenght of the string
+*/
+
+int _strlen(char *s)
+{
+	int result = 0;
+
+	while (*s != '\0')
+	{
+		result++;
+		s++;
+	}
+	return (result);
+}
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, writeT;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+	{
+		fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+		if (text_content != NULL)
+		{
+			writeT = write(fd, text_content, _strlen(text_content) + 1);
+			if (writeT == -1)
+				return (-1);
+		}
+	}
+	else
+	{
+		writeT = write(fd, text_content, _strlen(text_content));
+		if (writeT == -1)
+			return (-1);
+	}
+	return (1);
+}
