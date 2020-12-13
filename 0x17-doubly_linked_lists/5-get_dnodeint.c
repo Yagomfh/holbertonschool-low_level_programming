ee33344412486fb91c8464c9461a790fd9d7132f
yagomfh <yagomfh@gmail.com>
2020-12-13 10:08:25 +0000

    fix: index > listlen = NULL

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
-	if (head == NULL)
+	if (head->next == NULL)
 		return (NULL);
 	return (head);
 }
