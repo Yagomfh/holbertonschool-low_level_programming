9ccc0610ca509e71f51564549d89fa8c58880287
yagomfh <yagomfh@gmail.com>
2021-01-15 09:41:03 +0000

    fix: conditions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,7 +37,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (strlen(key) == 0)
+	if (strlen(key) == 0 || key == NULL)
+		return (0);
+	if (ht == NULL || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
