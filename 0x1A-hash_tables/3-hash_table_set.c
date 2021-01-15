7bfee571497536bfd0cc78832ef28a0d673da6a2
yagomfh <yagomfh@gmail.com>
2021-01-15 10:03:17 +0000

    fix: case when key/value is replaced

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,6 +44,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 
+	while (ht->array[index])
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = strdup(value);
+			return (1);
+		}
+	}
+
 	new_node = add_node(&(ht->array[index]), key, value);
 	if (new_node == NULL)
 		return (0);
