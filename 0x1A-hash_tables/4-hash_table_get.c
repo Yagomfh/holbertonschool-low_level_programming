6691167d00262e539739eb08a30d9f43dd7bc109
yagomfh <yagomfh@gmail.com>
2021-01-15 10:08:42 +0000

    fix: conditions

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,17 +1,22 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_get - retrieves a value associated with a key
-  * @ht: pointer to hash table
-  * @key: key to look for
-  * Return: value associated with the element
-  * or NULL if key couldn’t be found
-  */
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to hash table
+ * @key: key to look for
+ * Return: value associated with the element
+ * or NULL if key couldn’t be found
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *i_nodes;
+
+	if (key == NULL || strlen(key) == 0)
+		return (NULL);
+	if (ht == NULL)
+		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
 
