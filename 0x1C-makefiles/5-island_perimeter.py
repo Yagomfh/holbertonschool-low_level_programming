5a6171045baf57539be112a7f659b49e407cc928
yagomfh <yagomfh@gmail.com>
2021-02-19 14:16:37 +0000

    fix: adding documentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,17 @@
 def island_perimeter(grid):
     """Calculates the perimeter of an island"""
     island_beaches = 0
-    for y in range(1, len(grid) - 1):
-        for x in range(1, len(grid[0])):
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            count = 0
             if grid[y][x] == 1:
-                if grid[y - 1][x] == 0:
-                    island_beaches += 1
-                if grid[y + 1][x] == 0:
-                    island_beaches += 1
-                if grid[y][x - 1] == 0:
-                    island_beaches += 1
-                if grid[y][x + 1] == 0:
-                    island_beaches += 1
+                if y > 0:
+                    count += grid[i - 1][j]
+                if y < (len(grid) - 1):
+                    count += grid[i + 1][j]
+                if x > 0:
+                    count += grid[i][j - 1]
+                if x < (len(grid[0]) - 1):
+                    count += grid[i][j + 1]
+                island_beaches += 4 - count
     return island_beaches
