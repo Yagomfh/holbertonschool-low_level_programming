0759162f4ee29f87c64b074f4d39b1d53c0dfeb0
yagomfh <yagomfh@gmail.com>
2021-04-21 06:14:54 +0000

    fix: iterative version

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,48 +1,40 @@
 #include "search_algos.h"
 
 /**
-  * interpolationSearch - interpolation search algorithm
-  * @arr: array to look in to
-  * @lo: lowest index
-  * @hi: highest index
-  * @x: value to find
-  * @size: size of array
-  * Return: index where x was found or -1
-  */
-
-int interpolationSearch(int arr[], int lo, int hi, int x, size_t size)
-{
-	size_t pos = lo + (((double)(hi - lo) / (arr[hi] - arr[lo]))
-			* (x - arr[lo]));
-	if (!arr)
-		return (-1);
-	if (pos > size)
-		printf("Value checked array[%lu] is out of range\n", pos);
-	else
-		printf("Value checked array[%lu] = [%d]\n", pos, arr[pos]);
-	if (lo <= hi && x >= arr[lo] && x <= arr[hi])
-	{
-		if (arr[pos] == x)
-			return (pos);
-
-		if (arr[pos] < x)
-			return (interpolationSearch(arr, pos + 1, hi, x, size));
-
-		if (arr[pos] > x)
-			return (interpolationSearch(arr, lo, pos - 1, x, size));
-	}
-	return (-1);
-}
-
-/**
-  * interpolation_search - interpolation search algorithm
-  * @array: array to look in to
-  * @size: size of the array
-  * @value: value to look for
-  * Return: index where value was found
-  */
+ * interpolation_search - interpolation search algorithm
+ * @array: array to look in to
+ * @size: size of the array
+ * @value: value to look for
+ * Return: index where value was found
+ */
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	return (interpolationSearch(array, 0, (int)size - 1, value, size));
+	size_t l = 0;
+	size_t h = size - 1;
+	long int m;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	while (array[h] != array[l])
+	{
+		m = l + ((double)(h - l) / (array[h] - array[l])) * (value - array[l]);
+		if (m < 0 || m > (long int)size)
+		{
+			printf("Value checked array[%li] is out of range\n", m);
+			return (-1);
+		}
+		printf("Value checked array[%li] = [%i]\n", m, array[m]);
+		if (array[m] == value)
+			return (m);
+		else if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			h = m - 1;
+	}
+
+	if (array[l] == value)
+		return (l);
+	return (-1);
 }
